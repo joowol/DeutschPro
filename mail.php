c04ffadf7bd8dce8eadc666079e7ab6fa29871ff
Kondratenko <joowol@gmail.com>
2015-12-15 08:21:27 +0300

    add mail.php

---
+++ mail.php
@@ -0,0 +1,11 @@
+<?php
+
+$recepient = "joowol@gmail.com";
+$sitemap = "Название сайта";
+
+$name = trim($_POST["name"]);
+$phone = trim($_POST["phone"]);
+$message = "Имя: $name \nТелефон: $phone \nТекст: $text";
+
+$pagetitle = "Новая заявка с сайта \"$sitemap\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
